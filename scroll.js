71c1db40172aa99cbf6ca6470d6137dde1d5377b
Kazi <uidboy01@gmail.com>
2023-09-19 00:34:26 +0600

    Basic layout

---
+++ scroll.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 30,
+  keyboard: {
+    enabled: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
